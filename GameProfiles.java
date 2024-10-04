a163db5e5b79610ed2880ff703b33b83e79a8a4b
Jasmine Tann <153488776+jstann@users.noreply.github.com>
2024-10-04 19:57:18 -0400

    Update GameProfiles.java

--- GameProfiles.java
+++ GameProfiles.java
@@ -1,60 +1,59 @@
+/**
+ * Jasmine Tann
+ * 10/01/2024
+ * COMP 167 Computer Programming and Design
+ */
+/**
+ * The GameProfiles file is a class that manages the player profiles.
+ * It also manages the filename for the profile, which can be used to load or store player profiles.
+ */
+
+
+
 package edu.ncat.brickbreakerbackend;
 
 import java.util.ArrayList;
-
 public class GameProfiles {
     private PlayerProfile selectedProfile;
     private PlayerProfile highGameProfile;
     private ArrayList<PlayerProfile> profiles;
     private String profileFileName;
-
     public GameProfiles() {
         this.selectedProfile = null;
         this.highGameProfile = null;
         this.profiles = new ArrayList<>();
         this.profileFileName = null;
     }
-
     public PlayerProfile getSelectedProfile() {
         return selectedProfile;
     }
-
     public void setSelectedProfile(PlayerProfile selectedProfile) {
         this.selectedProfile = selectedProfile;
     }
-
     public PlayerProfile getHighGameProfile() {
         return highGameProfile;
     }
-
     public void setHighGameProfile(PlayerProfile highGameProfile) {
         this.highGameProfile = highGameProfile;
     }
-
     public ArrayList<PlayerProfile> getPlayerProfiles() {
         return profiles;
     }
-
     public void setPlayerProfiles(ArrayList<PlayerProfile> playerProfiles) {
         this.profiles = playerProfiles;
     }
-
     public String getProfileFileName() {
         return profileFileName;
     }
-
     public void setProfileFileName(String profileFileName) {
         this.profileFileName = profileFileName;
     }
-
     /**
      * ArrayList Methods
      */
-
     public int getNumPlayerProfiles() {
         return profiles.size();
     }
-
     public PlayerProfile getPlayerProfile(int index) {
         return profiles.get(index);
     }
@@ -67,18 +66,12 @@ public class GameProfiles {
     public void removePlayerProfile(int index) {
         profiles.remove(index);
     }
-
-
-
     @Override
     public String toString() {
         StringBuilder levelstring = new StringBuilder();
         for (PlayerProfile profile : profiles) {
             levelstring.append(profile.toString());
         }
-
-
-
         return levelstring.toString();
     }
 }
